48f12057bc6aad1960497b17352bcbc7d9b4b21b
Ángel Brasero <angel.brasero@gmail.com>
2020-06-26 19:43:35 +0200

    Added netlify identity

--- pages/index.vue
+++ pages/index.vue
@@ -32,3 +32,13 @@ article
         p A, A#, B, C, C#...
 
 </template>
+
+<script>
+export default {
+  head () {
+    return {
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+    }
+  }
+}
+</script>
