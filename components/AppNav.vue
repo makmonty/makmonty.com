1d874352fd4732cf3ae387168003205a190b1288
Ángel Brasero <angel.brasero@gmail.com>
2020-09-01 16:35:51 +0200

    Doing some configuration and base components

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,22 +1,38 @@
 <template>
-  <nav class="flex p-6 container mx-auto">
-    <ul class="flex">
-      <li>
-        <nuxt-link to="/">
-          Home
-        </nuxt-link>
-      </li>
-      <li>
-        <nuxt-link to="/cv">
-          CV
-        </nuxt-link>
-      </li>
-    <!-- //- li
-    //-   a(href="portfolio.pug") Portfolio
-    //- li
-    //-   a(href="#contact") Contact
-    //- li
-    //-   a(href="http://blog.makmonty.com") Blog -->
-    </ul>
-  </nav>
+  <div class="border-b border-teal-500">
+    <nav class="flex p-6 container mx-auto">
+      <ul class="flex space-x-6">
+        <li v-for="item in menu" :key="item.to">
+          <nuxt-link :to="item.to" class="hover:border-b-4">
+            {{ item.name }}
+          </nuxt-link>
+        </li>
+      <!-- //- li
+      //-   a(href="portfolio.pug") Portfolio
+      //- li
+      //-   a(href="#contact") Contact
+      //- li
+      //-   a(href="http://blog.makmonty.com") Blog -->
+      </ul>
+    </nav>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      menu: [
+        {
+          name: 'Home',
+          to: '/'
+        },
+        {
+          name: 'CV',
+          to: '/cv'
+        }
+      ]
+    }
+  }
+}
+</script>
