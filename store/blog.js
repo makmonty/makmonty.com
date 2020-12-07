3247df2871813c37100bdef7999173b783d1c681
Ángel Brasero Moreno <angel.brasero@gmail.com>
2020-12-07 23:26:28 +0100

    Draft blog posts

--- store/blog.js
+++ store/blog.js
@@ -10,6 +10,9 @@ export const mutations = {
 
 export const getters = {
   byCategory(state) {
-    return category => state.items.filter(i => (i.categories || []).includes(category));
+    return category => state.items.filter(i => !i.draft && (i.categories || []).includes(category));
+  },
+  publishedItems(state) {
+    return state.items.filter(i => !i.draft);
   }
 };
