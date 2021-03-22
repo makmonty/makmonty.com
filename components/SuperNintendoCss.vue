31ae917378a525c2ecfdd956858ec27db45c7308
Ángel Brasero Moreno <angel.brasero@gmail.com>
2021-03-22 18:25:41 +0100

    Snes buttons

--- components/SuperNintendoCss.vue
+++ components/SuperNintendoCss.vue
@@ -13,6 +13,17 @@
             </div>
           </div>
           <div class="snes-slot" />
+          <div class="snes-buttons">
+            <div class="snes-power-button snes-button snes-border-button">
+              <span>Power</span>
+            </div>
+            <div class="snes-eject-button snes-button">
+              <span>Eject</span>
+            </div>
+            <div class="snes-reset-button snes-button snes-border-button">
+              <span>Reset</span>
+            </div>
+          </div>
         </div>
         <div class="snes-bottom-plate snes-plate" />
       </div>
@@ -21,18 +32,19 @@
 </template>
 
 <style lang="scss" scoped>
-$snes-color-light: #ddd;
-$snes-color-grey: #939393;
+$snes-color-light: #ccc;
+$snes-color-grey: #909090;
 $snes-color-dark: #606060;
 $groove-color: #aaa;
 // 200mm x 72mm x 242mm
 $real-snes-width: 200; // mm
 $real-snes-height: 242; // mm
 $snes-ratio: $real-snes-height / $real-snes-width;
-$snes-width: 500px;
+$snes-width: 500em;
 $border-groove: 2px groove $groove-color;
 
 .snes {
+  font-size: 1px;
   width: $snes-width;
   height: $snes-width * $snes-ratio;
 
@@ -44,12 +56,12 @@ $border-groove: 2px groove $groove-color;
     background: linear-gradient(
       180deg,
       $snes-color-light 60%,
-      darken($snes-color-light, 13%) 80%
+      darken($snes-color-light, 10%) 80%
     );
     border-radius: $snes-width * 0.08;
     box-shadow:
-      inset -4px -4px 4px rgba(0,0,0,0.15),
-      inset 4px 4px 4px rgba(255,255,255,0.2);
+      inset -4em -4em 4em rgba(0,0,0,0.15),
+      inset 4em 4em 4em rgba(255,255,255,0.3);
 
     .snes-bevel {
       width: 100%;
@@ -60,8 +72,8 @@ $border-groove: 2px groove $groove-color;
       flex-direction: column;
       align-items: center;
       box-shadow:
-        inset -4px -4px 4px rgba(0,0,0,0.15),
-        inset 4px 4px 4px rgba(255,255,255,0.2);
+        inset -4em -4em 4em rgba(0,0,0,0.15),
+        inset 4em 4em 4em rgba(255,255,255,0.3);
 
       .snes-plate {
         width: 70%;
@@ -86,7 +98,7 @@ $border-groove: 2px groove $groove-color;
           position: relative;
           width: 13%;
           border-radius: 50%;
-          border: 0.2px inset #aaa;
+          border: 0.2em inset #aaa;
 
           &:before {
             content: "";
@@ -101,15 +113,49 @@ $border-groove: 2px groove $groove-color;
         }
 
         .snes-slot {
-          margin-top: 10%;
+          margin-top: 15%;
           background: $snes-color-dark;
           background-clip: content-box;
-          height: 17%;
-          border: 12px inset rgba(200, 200, 200, 0.5);
-          border-radius: 20px 20px 50% 50% / 20px 20px 30px 30px;
+          height: 20%;
+          border: 12em inset rgba(200, 200, 200, 0.5);
+          border-radius: 20em 20em 40% 40% / 20em 20em 30em 30em;
           // box-shadow:
-          //   inset -12px -12px 2px rgba(255, 255, 255, 0.1),
-          //   inset 12px 12px 2px rgba(255, 255, 255, 0.1),;
+          //   inset -12em -12em 2em rgba(255, 255, 255, 0.1),
+          //   inset 12em 12em 2em rgba(255, 255, 255, 0.1),;
+        }
+
+        .snes-buttons {
+          margin: 25% 3%;
+          margin-bottom: 0;
+          height: 22%;
+          display: flex;
+          justify-content: space-between;
+
+          .snes-button {
+            border-radius: 10em;
+            border-width: 6em;
+            display:flex;
+            text-align: center;
+            color: $snes-color-light;
+
+            span {
+              font-size: 14em;
+            }
+          }
+
+          .snes-border-button {
+            width: 70em;
+            border-style: solid;
+            border-color: $snes-color-light;
+            background: $snes-color-dark;
+          }
+
+          .snes-eject-button {
+            width: 160em;
+            background-clip: content-box;
+            border-style: inset;
+            border-color: rgba(200, 200, 200, 0.5);
+          }
         }
       }
 
