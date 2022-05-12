809e23a9d7f11163808012e5109902cea244a2a7
Ángel Brasero Moreno <angel.brasero@gmail.com>
2022-05-12 13:11:47 +0200

    Migrating AppNav to composition API

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,3 +1,32 @@
+<script setup>
+const menu = [
+  {
+    name: 'Home',
+    to: '/'
+  },
+  {
+    name: 'CV',
+    to: '/cv'
+  },
+  {
+    name: 'Projects',
+    to: '/projects'
+  },
+  // {
+  //   name: 'Portfolio',
+  //   to: '/portfolio'
+  // },
+  // {
+  //   name: 'Contact',
+  //   to: '/contact'
+  // },
+  // {
+  //   name: 'Blog',
+  //   href: 'http://blog.makmonty.com'
+  // }
+];
+</script>
+
 <template>
   <nav class="flex container mx-auto items-center px-8">
     <ul class="flex p-8 space-x-8 justify-center w-full">
@@ -20,38 +49,3 @@
     </ul>
   </nav>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      menu: [
-        {
-          name: 'Home',
-          to: '/'
-        },
-        {
-          name: 'CV',
-          to: '/cv'
-        },
-        {
-          name: 'Projects',
-          to: '/projects'
-        },
-        // {
-        //   name: 'Portfolio',
-        //   to: '/portfolio'
-        // },
-        // {
-        //   name: 'Contact',
-        //   to: '/contact'
-        // },
-        // {
-        //   name: 'Blog',
-        //   href: 'http://blog.makmonty.com'
-        // }
-      ]
-    };
-  }
-};
-</script>
