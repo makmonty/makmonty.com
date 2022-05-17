fe85c9555d3d51436b828e791a40d29475b30ad3
Ángel Brasero Moreno <angel.brasero@qonto.com>
2022-05-18 00:38:11 +0200

    Maze generator almost finished

--- lib/maze.ts
+++ lib/maze.ts
@@ -1,3 +1,5 @@
+import {shuffle} from "./array";
+
 export enum Direction {
   LEFT = 'left',
   RIGHT = 'right',
@@ -10,7 +12,18 @@ export class MazeCell {
   [Direction.DOWN]: boolean = false;
   [Direction.RIGHT]: boolean = false;
   [Direction.LEFT]: boolean = false;
+
+  get visited() {
+    return this[Direction.UP] || this[Direction.DOWN] || this[Direction.LEFT] || this[Direction.RIGHT];
+  }
 }
+
+const DirectionOposites = {
+  [Direction.UP]: Direction.DOWN,
+  [Direction.DOWN]: Direction.UP,
+  [Direction.RIGHT]: Direction.LEFT,
+  [Direction.LEFT]: Direction.RIGHT,
+};
 
 export class Maze {
 
@@ -18,51 +31,59 @@ export class Maze {
 
   constructor(private width: number, private height: number) {
     this.cells = Array.from(Array(this.width), () =>
-      Array(this.height).fill(null)
+      Array.from(Array(this.height), () => new MazeCell())
     );
   }
 
-  generate() {
-    for (let y = 0; y < this.height; y++) {
-      for (let x = 0; x < this.width; x++) {
-        const cell = new MazeCell();
-        if (x < this.width - 1) {
-          cell.right = true;
-        }
-        if (x > 0) {
-          cell.left = true;
-        }
-        if (y < this.height - 1) {
-          cell.down = true;
-        }
-        if (y > 0) {
-          cell.up = true;
-        }
-        this.cells[x][y] = cell;
-      }
-    }
-  }
-
-  iterate(x: number, y: number) {
+  generate(x: number = 0, y: number = 0) {
     const cell = this.cells[x][y];
+    const directions = this.getAvailableDirections(x, y);
+    shuffle(directions);
 
-    const neighbours = this.getNeighbours(x, y);
-
-    if (neighbours.length) {
-      const nextIndex = Math.floor(Math.random() * neighbours.length);
-      const nextKey = neighbours[nextIndex];
-      cell[nextKey] = true;
-      return
-    }
-    return null;
+    directions.forEach(direction => {
+      const neighbourCell = this.getNeighbourCell(x, y, direction);
+      if (!neighbourCell.visited) {
+        cell[direction] = true;
+        neighbourCell[DirectionOposites[direction]] = true;
+        const [newX, newY] = this.getNeighbourCoordinates(x, y, direction)
+        this.generate(newX, newY);
+      }
+    });
   }
 
-  getNeighbours(x: number, y: number) {
+  getAvailableDirections(x: number, y: number) {
     return [
       ...(x > 0 ? [Direction.LEFT] : []),
       ...(x < this.width - 1 ? [Direction.RIGHT] : []),
       ...(y > 0 ? [Direction.UP] : []),
       ...(y < this.height - 1 ? [Direction.DOWN] : []),
     ];
+  }
+
+  getNeighbourCell(x: number, y: number, direction: Direction) {
+    const [newX, newY] = this.getNeighbourCoordinates(x, y, direction);
+    return this.cells[newX][newY];
+  }
+
+  getNeighbourCoordinates(x: number, y: number, direction: Direction) {
+    let newX = x;
+    let newY = y;
+
+    switch(direction) {
+      case Direction.LEFT:
+        newX--;
+        break;
+      case Direction.RIGHT:
+        newX++;
+        break;
+      case Direction.UP:
+        newY--;
+        break;
+      case Direction.DOWN:
+        newY++;
+        break;
+    }
+
+    return [newX, newY];
   }
 }
