3e7cf06e2c3a97eb8e2b961a2b25ce94faa3eaed
Ángel Brasero Moreno <angel.brasero@gmail.com>
2024-01-12 10:37:59 +0100

    Astro version ready

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="astro/client" />
+// declare const netlifyIdentity: any;
+interface Window {
+  netlifyIdentity: {
+    on: (event: string, callback: (...args: any) => void) => void
+  };
+}
