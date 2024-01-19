6e4cccfda9a357287e368251631415dcdb2e1fc0
Ángel Brasero Moreno <angel.brasero@gmail.com>
2024-01-19 14:11:01 +0100

    Added sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://makmonty.com',
+  integrations: [sitemap()]
 });
