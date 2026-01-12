4c36610388dcdd947b84e50dd77a4a4c04eefe8c
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:53 +0100

    Defering some script tags

--- pages/index.vue
+++ pages/index.vue
@@ -90,7 +90,7 @@ export default {
   },
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js', defer: true }]
     };
   }
 };
