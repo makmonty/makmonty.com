21f9839b4490d62171b30ee6302573dc8441c232
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:04:48 +0100

    Diseño nuevo

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'makmonty video games' });
+  res.render('index', { title: 'makmonty video games', hideNavbar: true });
 });
 
 // router.get('/games', function(req, res, next) {
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
 // });
 
 router.get('/games/box-rain', function(req, res, next) {
-  res.render('games/box-rain', { title: 'Box Rain - makmonty' });
+  res.render('games/box-rain', { title: 'Box Rain - makmonty', breadcrumb: 'Box Rain' });
 });
 
 
