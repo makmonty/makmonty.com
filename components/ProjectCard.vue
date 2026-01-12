bd6667cf479986f1cd13e15129f81eb34de75393
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:53 +0100

    Poco avance

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -22,7 +22,7 @@ export default {
 <style lang="scss" scoped>
 .card-image {
   background-image: url('~assets/images/gamepad.png');
-  background-size: 25%;
-  image-rendering: crisp-edges;
+  background-size: 20%;
+  // image-rendering: crisp-edges;
 }
 </style>
