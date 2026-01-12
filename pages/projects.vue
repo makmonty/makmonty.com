0c9a7e99dd5e4b9e53038793e246e57a4e06728b
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:55 +0100

    Added promo image for maze

--- pages/projects.vue
+++ pages/projects.vue
@@ -2,16 +2,18 @@
   <article class="container mx-auto px-8 my-24 ">
     <section>
       <h2>Web experiments</h2>
-      <NuxtLink
-        v-for="item in webExperiments"
-        :key="item.slug"
-        :to="'/' + item.slug"
-      >
-        <ProjectCard
-          class="my-2"
-          :project="item"
-        />
-      </NuxtLink>
+      <div class="flex flex-col items-center lg:flex-row flex-wrap justify-center space-y-8 lg:space-y-0 lg:space-x-12">
+        <NuxtLink
+          v-for="item in webExperiments"
+          :key="item.slug"
+          :to="'/' + item.slug"
+        >
+          <ProjectCard
+            class="my-2"
+            :project="item"
+          />
+        </NuxtLink>
+      </div>
     </section>
 
     <section>
@@ -37,7 +39,7 @@ const webExperiments = [
   {
     title: 'Maze',
     slug: 'maze',
-    featuredImage: '/cms/img/3dbird-promo.jpg',
+    featuredImage: '/cms/img/maze-promo.jpg',
   }
 ];
 const games = await useGames();
