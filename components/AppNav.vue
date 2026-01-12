f8846ad89de77d6317adaddaa29a2a116169b6ae
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:52 +0100

    Using tailwind

---
+++ components/AppNav.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="flex p-6 container mx-auto">
+    <ul class="flex">
+      <li>
+        <nuxt-link to="/">
+          Home
+        </nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/cv">
+          CV
+        </nuxt-link>
+      </li>
+    <!-- //- li
+    //-   a(href="portfolio.pug") Portfolio
+    //- li
+    //-   a(href="#contact") Contact
+    //- li
+    //-   a(href="http://blog.makmonty.com") Blog -->
+    </ul>
+  </nav>
+</template>
