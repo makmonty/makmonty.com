eebcedc22be48148be1422f86c4664a09faaa9d2
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:04:48 +0100

    Algo para ir empezando a publicar

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('games', { title: 'makmonty video games' });
+  res.render('index', { title: 'makmonty video games' });
 });
 
 // router.get('/games', function(req, res, next) {
 //   res.render('games', { title: 'Games - makmonty' });
 // });
+
+router.get('/games/box-rain', function(req, res, next) {
+  res.render('games/box-rain', { title: 'Box Rain - makmonty' });
+});
 
 
 module.exports = router;
