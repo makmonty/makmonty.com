7d0ddcdfe1805e9a61ca9439b38a6cc0e2337726
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:52 +0100

    Changing design

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,26 @@
 <template>
-  <article class="flex flex-col space-y-12 py-12">
-    <section class="container mx-auto px-8 flex">
-      <img src="../assets/images/yo-24x24.png" class="w-24 h-24 object-contain rendering-crisp-edges">
-      <div class="ml-6">
-        <h1 class="text-4xl font-bold">
-          I'm Ángel Brasero
-        </h1>
-        <p class="summary">
-          I'm a software engineer. Passionate about technology and software development.<br>
-          I spend most of my time coding.
-        </p>
-        <div class="mt-4">
-          <nuxt-link
-            v-slot="{ navigate }"
-            to="/cv"
-          >
-            <BaseButton @click="navigate">
-              Check my CV
-            </BaseButton>
-          </nuxt-link>
+  <article class="flex flex-col space-y-24 pb-24">
+    <section class="container mx-auto px-8 h-screen flex justify-center items-center">
+      <div class="flex text-xl">
+        <img src="../assets/images/yo-24x24.png" class="w-64 h-64 p-4 object-contain rendering-crisp-edges border rounded-full border-primary">
+        <div class="ml-6 flex flex-col justify-center">
+          <h1 class="text-4xl">
+            I'm <span class="text-primary">Ángel Brasero</span>
+          </h1>
+          <p class="summary">
+            I'm a software engineer. Passionate about technology and software development.<br>
+            I spend most of my time coding.
+          </p>
+          <div class="mt-4">
+            <nuxt-link
+              v-slot="{ navigate }"
+              to="/cv"
+            >
+              <BaseButton @click="navigate">
+                Check my CV
+              </BaseButton>
+            </nuxt-link>
+          </div>
         </div>
       </div>
     </section>
