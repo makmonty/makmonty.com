930e8f7955a484467414bfde5ab705c592a329e2
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:55 +0100

    Starting MazeGenerator component

---
+++ components/MazeGenerator.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {Maze} from '~~/lib/maze';
+
+const maze = new Maze(3, 3);
+maze.generate();
+</script>
+
+<template>
+  <div>
+    <table>
+      <tr v-for="rows in maze.cells"></tr>
+    </table>
+  </div>
+</template>
