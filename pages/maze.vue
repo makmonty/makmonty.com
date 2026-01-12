0d7738c72b9790dcaf11f68f2b15ba1ff8dcc944
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:55 +0100

    Configuring the maze from some inputs

--- pages/maze.vue
+++ pages/maze.vue
@@ -1,9 +1,26 @@
+<script lang="ts" setup>
+const cols = ref('20');
+const rows = ref('20');
+const cellSize = ref('20');
+</script>
+
 <template>
   <article class="container mx-auto">
+    <div>
+      Cols <FormInput type="number" v-model="cols" />
+    </div>
+    <div>
+      Rows <FormInput type="number" v-model="rows" />
+    </div>
+    <div>
+      Cell size <FormInput type="number" v-model="cellSize" />
+    </div>
+
     <MazeGenerator
       class="mx-auto"
-      :cols="20"
-      :rows="20"
+      :cols="parseInt(cols)"
+      :rows="parseInt(rows)"
+      :cellSize="parseInt(cellSize)"
     />
   </article>
 </template>
