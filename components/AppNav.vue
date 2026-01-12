693daff16b76fdf4d589638885675086f3f749db
Ángel Brasero <372376+makmonty@users.noreply.github.com>
2026-01-12 17:07:52 +0100

    responsiveness in index

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -1,21 +1,19 @@
 <template>
-  <div class="border-b border-teal-500">
-    <nav class="flex p-8 container mx-auto">
-      <ul class="flex space-x-8">
-        <li v-for="item in menu" :key="item.to">
-          <nuxt-link :to="item.to" class="hover:border-b-4">
-            {{ item.name }}
-          </nuxt-link>
-        </li>
-      <!-- //- li
-      //-   a(href="portfolio.pug") Portfolio
-      //- li
-      //-   a(href="#contact") Contact
-      //- li
-      //-   a(href="http://blog.makmonty.com") Blog -->
-      </ul>
-    </nav>
-  </div>
+  <nav class="flex p-8 container mx-auto">
+    <ul class="flex space-x-8">
+      <li v-for="item in menu" :key="item.to">
+        <nuxt-link :to="item.to" class="hover:border-b-4">
+          {{ item.name }}
+        </nuxt-link>
+      </li>
+    <!-- //- li
+    //-   a(href="portfolio.pug") Portfolio
+    //- li
+    //-   a(href="#contact") Contact
+    //- li
+    //-   a(href="http://blog.makmonty.com") Blog -->
+    </ul>
+  </nav>
 </template>
 
 <script>
